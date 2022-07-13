c10c2db3f38e86ee9b40d30387c65e90f1332c0a
Jesse Songok <jessesongok@gmail.com>
2022-07-13 11:53:04 +0300

    Project Structure

---
+++ src/components/Search.svelte
@@ -0,0 +1,11 @@
+<script>
+    
+</script>
+<div class="form-group">
+    <input 
+    name="search" 
+    type="text" 
+    placeholder="Search for House"
+    class="form-control input input-bordered w-full"
+    />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Search from "../components/Search.svelte";
+    import HouseList from "../components/HouseList.svelte";
+</script>
+<div class="container mx-auto px-4">
+
+    <Search/>
+
+    <HouseList/>
+
+
+
+
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
